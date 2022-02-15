2d7f3892f23a4e14fc838d84eae5dcaa9d58a694
Joao Victor <joaogmavictor@gmail.com>
2022-02-15 12:04:01 -0300

    Refactor: Removendo Test App

--- src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import App from '../App';
-
-test('renders ToDo List', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/ToDo List/i);
-  expect(linkElement).toBeInTheDocument();
-});
